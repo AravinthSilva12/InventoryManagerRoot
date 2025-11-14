b7505dad2fad14882368143af6815c95b458cd05
Aravinthasamy Rajkumar <aravinthasamy281099@gmail.com>
2025-11-14 19:08:35 +0530

    Merge remote-tracking branch 'InventoryManagerRoot/master' both names for remote updated. Blueprint created. classes for model segment is made.

---
+++ src/model/StockHistory.java
@@ -0,0 +1,12 @@
+package model;
+
+public class StockHistory {
+    // variables & properties :
+    int id;
+    int stockItemId;
+    String type;
+    long timestamp;
+    int changeQuantity;
+    }
+
+
---
+++ src/model/StockItem.java
@@ -0,0 +1,12 @@
+package model;
+
+public class StockItem {
+    // variables & properties :
+    int id;
+    String name;
+    int quantity;
+    int lowStockThreshold;
+    long lastUpdated;
+    }
+
+
