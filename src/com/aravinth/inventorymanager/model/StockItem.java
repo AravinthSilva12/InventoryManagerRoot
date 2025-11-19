934a41039a2e17b62506e5785c78fb5f6c109340
Aravinthasamy Rajkumar <aravinthasamy281099@gmail.com>
2025-11-19 13:47:15 +0530

    Merge remote-tracking branch 'InventoryManagerRoot/master' both names for remote updated. Blueprint created. classes for model segment is made. 1. Interfaces created and put inside repository package. inmemory package is created with two in memory classes. Nov 19th updated work.

--- src/com/aravinth/inventorymanager/model/StockItem.java
+++ src/com/aravinth/inventorymanager/model/StockItem.java
@@ -2,11 +2,70 @@ package com.aravinth.inventorymanager.model;
 
 public class StockItem {
     // variables & properties :
-    int id;
-    String name;
-    int quantity;
-    int lowStockThreshold;
-    long lastUpdated;
+    private int id;
+    private String name;
+    private int quantity;
+    private int lowStockThreshold;
+    private long lastUpdated;
+    // -------------------------
+    // constructor :
+    public StockItem(int id, String name, int quantity, int lowStockThreshold, long lastUpdated){
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.lowStockThreshold = lowStockThreshold;
+        this.lastUpdated = lastUpdated;
     }
+    // -------------------------
+    // id methods :
+    public int getId(){
+      return id;
+    }
+
+    public void setId(int id){
+      this.id = id;
+    }
+    // -------------------------
+    // quantity methods :
+    public int getQuantity(){
+       return quantity;
+    }
+
+    public void  setQuantity(int quantity){
+       this.quantity = quantity;
+    }
+    // -------------------------
+    // getLowStockThreshold methods :
+    public int getLowStockThreshold(){
+        return lowStockThreshold;
+    }
+
+    public void setLowStockThreshold(int lowStockThreshold){
+      this.lowStockThreshold = lowStockThreshold;
+    }
+    // -------------------------
+    public long getLastUpdated(){
+       return lastUpdated;
+    }
+
+    public void setLastUpdated(long lastUpdated){
+       this.lastUpdated = lastUpdated;
+    }
+    // -------------------------
+    public boolean isLowStock(){
+       return quantity <= lowStockThreshold;
+    }
+    // -------------------------
+    // lastUpdated method :
+    @Override
+    public String toString(){
+        return "StockItem {" +
+                "Id = " + id +
+                ", Name : " + name +
+                ", Quantity = " + quantity +
+                ", LowStockThreshold = " + lowStockThreshold +
+                ", Last updated : " + lastUpdated + " }";
+    }
+}
 
 
