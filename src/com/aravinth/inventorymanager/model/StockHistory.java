afca65df101d485994b25b82e7ed5a7e11ebb4d5
Aravinthasamy Rajkumar <aravinthasamy281099@gmail.com>
2025-11-19 22:31:43 +0530

    Resolve merge conflict in StockHistory.java

--- src/com/aravinth/inventorymanager/model/StockHistory.java
+++ src/com/aravinth/inventorymanager/model/StockHistory.java
@@ -1,14 +1,15 @@
 package com.aravinth.inventorymanager.model;
 
 public class StockHistory {
-    // variables & properties :
+    // variables & properties
     private int id;
     private int stockItemId;
     private String type;
     private long timestamp;
     private int quantityChange;
     private String note;
-    // constructor :
+
+    // constructor
     public StockHistory(int id, int stockItemId, String type, long timestamp, int quantityChange, String note){
         this.id = id;
         this.stockItemId = stockItemId;
@@ -17,65 +18,64 @@ public class StockHistory {
         this.quantityChange = quantityChange;
         this.note = note;
     }
-    // id methods :
+
+    // id
     public int getId(){
         return id;
     }
-
     public void setId(int id){
         this.id = id;
     }
-    // stockItemId methods :
+
+    // stockItemId
     public int getStockItemId(){
         return stockItemId;
     }
-
     public void setStockItemId(int stockItemId){
         this.stockItemId = stockItemId;
     }
-    // type methods :
+
+    // type
     public String getType(){
         return type;
     }
-
     public void setType(String type){
         this.type = type;
     }
-    // timestamp methods :
+
+    // timestamp
     public long getTimestamp(){
         return timestamp;
     }
-
     public void setTimestamp(long timestamp){
         this.timestamp = timestamp;
     }
-    // quantityChange methods :
+
+    // quantityChange
     public int getQuantityChange(){
         return quantityChange;
     }
-
     public void setQuantityChange(int quantityChange){
         this.quantityChange = quantityChange;
     }
-    // note method :
+
+    // note
     public String getNote(){
         return note;
     }
-
     public void setNote(String note){
         this.note = note;
     }
-    // conclusion display method :
+
     @Override
     public String toString(){
-         return "StockHistory{" +
-                 "id = "+ id +
-                 ",stockItemId =" + stockItemId +
-                 ",type = " + type +
-                 ", quantityChange = " + quantityChange +
-                 ", timeStamp = " + timestamp +
-                 ", note : " + note + " }";
+        return "StockHistory{" +
+                "id=" + id +
+                ", stockItemId=" + stockItemId +
+                ", type='" + type + '\'' +
+                ", quantityChange=" + quantityChange +
+                ", timestamp=" + timestamp +
+                ", note='" + note + '\'' +
+                '}';
     }
 }
-
-
