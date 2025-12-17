0672ef298a0688de1d574190587053e4e3e00cc3
Aravinthasamy Rajkumar <aravinthasamy281099@gmail.com>
2025-12-17 21:49:43 +0530

    Merge remote-tracking branch 'InventoryManagerRoot/master' both names for remote updated. Blueprint created. classes for model segment is made. 1. Interfaces created and put inside repository package. inmemory package is created with two in memory classes Nov 19th updated work. InMemoryStockItemRepository class is fully finished and updated. StockHistory class fully finished & updated. final update of the day Nov21st : Stock history segment is finished and updated. Nov22nd : Service interfaces are made and updated. Project resumed today (17.12.2025).

--- src/com/aravinth/inventorymanager/service/InventoryManagerService.java
+++ src/com/aravinth/inventorymanager/service/InventoryManagerService.java
@@ -1,4 +1,24 @@
 package com.aravinth.inventorymanager.service;
+import com.aravinth.inventorymanager.service.StockHistoryService;
+import com.aravinth.inventorymanager.service.StockItemService;
 
 public class InventoryManagerService {
+     void addInventory(){
+
+     }
+
+     void viewInventoryItem(){
+
+     }
+
+
+
+
+
+
+
+
+
+
+
 }
