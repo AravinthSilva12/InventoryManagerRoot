6257fda03354612eb1f6195b5100e7d2c7b5d13d
Aravinthasamy Rajkumar <aravinthasamy281099@gmail.com>
2025-12-19 13:02:52 +0530

    Merge remote-tracking branch 'InventoryManagerRoot/master' both names for remote updated. Blueprint created. classes for model segment is made. 1. Interfaces created and put inside repository package. inmemory package is created with two in memory classes Nov 19th updated work. InMemoryStockItemRepository class is fully finished and updated. StockHistory class fully finished & updated. final update of the day Nov21st : Stock history segment is finished and updated. Nov22nd : Service interfaces are made and updated. Project resumed today (17.12.2025). Today - the InventoryManagerService class infrastucture is setup completely (need to finish the place holding methods tomorrow). (19.12.2025).

--- src/com/aravinth/inventorymanager/service/InventoryManagerService.java
+++ src/com/aravinth/inventorymanager/service/InventoryManagerService.java
@@ -1,24 +1,47 @@
 package com.aravinth.inventorymanager.service;
-import com.aravinth.inventorymanager.service.StockHistoryService;
-import com.aravinth.inventorymanager.service.StockItemService;
+import com.aravinth.inventorymanager.model.StockItem;
+import com.aravinth.inventorymanager.model.StockHistory;
+import com.aravinth.inventorymanager.repository.StockHistoryRepository;
+import com.aravinth.inventorymanager.repository.StockItemRepository;
+import java.util.List;
 
 public class InventoryManagerService {
-     void addInventory(){
+    private StockHistoryRepository historyRepo;
+    private StockItemRepository stockRepo;
 
+    public InventoryManagerService(StockItemRepository stockRepo, StockHistoryRepository historyRepo){
+        this.stockRepo = stockRepo;
+        this.historyRepo = historyRepo;
+    }
+
+    public StockItem addStockItem(StockItem item){
+         if(item == null){
+
+         }
      }
 
-     void viewInventoryItem(){
-
+    public StockItem getStockItem(int id){
+        return null;
      }
 
+    public boolean deleteStockItem(int id){
+           return false;
+    }
 
+    public List<StockItem> getAllstockItems(){
+          return null;
+    }
 
+    public StockItem updateStockItem(int id, StockItem updatedItem){
+       return null;
+    }
+// Stock History part below :
+    public void logStockHistory(StockHistory history){
 
+    }
 
-
-
-
-
-
+    public List<StockHistory> viewStockHistory(int itemId){
+        return null;
+    }
 
 }
